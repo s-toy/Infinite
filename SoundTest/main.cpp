7d5dc9d5fe412871d65e9b7e1922ab3c661457ae
ikutoo <1007833641@qq.com>
2018-10-04 12:23:49 +0800

    add head.

--- SoundTest/main.cpp
+++ SoundTest/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	BOOL flag = PlaySound("G:\\Resources\\Sounds\\31.wav", NULL, SND_ASYNC | SND_LOOP);
+	BOOL flag = PlaySound(".\\31.wav", NULL, SND_ASYNC | SND_LOOP);
 
 	getchar();
 
